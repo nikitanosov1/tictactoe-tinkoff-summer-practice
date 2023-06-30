891b1194f357172a54f22f94e89d6f18ecc6aa19
nikitanosov1 <nikinosov2002@mail.ru>
2023-06-30 22:45:33 +0400

    init

---
+++ src/main/java/ru/tinkoff/tictactoe/ping/PingController.java
@@ -0,0 +1,12 @@
+package ru.tinkoff.tictactoe.ping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
---
+++ src/test/java/ru/tinkoff/tictactoe/TictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
