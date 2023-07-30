31ca84097363718e70f478f3e0a02632bf63df8e
nikitanosov1 <nikinosov2002@mail.ru>
2023-07-30 20:54:15 +0400

    Добавил тест contextLoads

---
+++ src/test/java/ru/tinkoff/tictactoe/integration/test/TictactoeApplicationTests.java
@@ -0,0 +1,10 @@
+package ru.tinkoff.tictactoe.integration.test;
+
+import org.junit.jupiter.api.Test;
+import ru.tinkoff.tictactoe.integration.IntegrationSettings;
+
+class TictactoeApplicationTests extends IntegrationSettings {
+    @Test
+    void contextLoads() {
+    }
+}
