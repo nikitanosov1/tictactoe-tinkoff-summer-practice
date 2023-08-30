a125fb63d0d8423dc1da23a8029a2466bbd995d3
nikitanosov1 <nikinosov2002@mail.ru>
2023-08-30 14:01:48 +0400

    Убрал неиспользуемые зависимости

--- src/main/java/ru/tinkoff/tictactoe/client/BotClient.java
+++ src/main/java/ru/tinkoff/tictactoe/client/BotClient.java
@@ -1,12 +1,10 @@
 package ru.tinkoff.tictactoe.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.net.URI;
-import java.util.UUID;
 
 @FeignClient(name = "botClient", url = "${botClient.ribbon.listOfServers}")
 public interface BotClient {
